5e94d403b51d22787835acc235f3611adc6823bb
manxz105 <www.debil.ru@yandex.ru>
2025-08-12 18:31:25 +0500

    Create main.py
    train

---
+++ main.py
@@ -0,0 +1,8 @@
+import cv2
+weights = [
+  "YOUR_WEIGHTS_HERE"
+]
+net = cv2.dnn.readNet(weights)
+class Training:
+  layer_name = net.getLayersName()
+  output_layers = [layer_name[i[0] - 1] for i in net.getUnconnectedOutLayers()]
