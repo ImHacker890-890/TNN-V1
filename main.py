ca4d36c963ed335e988dc8a78d89fae0f39dea12
manxz105 <www.debil.ru@yandex.ru>
2025-08-12 18:43:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ weights = [
 ]
 net = cv2.dnn.readNet(weights)
 class Training:
-  layer_name = net.getLayersName()
-  output_layers = [layer_name[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+  def train():
+    layer_name = net.getLayersName()
+    output_layers = [layer_name[i[0] - 1] for i in net.getUnconnectedOutLayers()]
